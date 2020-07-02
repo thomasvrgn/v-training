fd68a0e111b5ac7a2dc5566a99aa9741e67e2b4c
NessMC <contact@nessmc.fr>
2020-07-02 13:48:11 +0200

    🔨 Factorial function added.

--- examples/functions.v
+++ examples/functions.v
@@ -16,8 +16,24 @@ fn add (numbers []int) int {
 	return result
 }
 
+fn factorial (number int) int {
+	mut factorial_num := 1
+	
+	for i := 1; i < number + 1; i++ {
+		factorial_num *= i
+	}
+
+
+	return factorial_num
+
+}
+
 fn main () {
 	say("Hello world!")
+
 	list := [0, 5, 10, 15, 20, 25]
 	println(add(list))
+
+	number := 5
+	println(factorial(number))
 }
