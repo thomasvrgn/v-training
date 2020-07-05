417c210a51702c9918498eb84d989f9f91057761
NessMC <contact@nessmc.fr>
2020-07-05 21:04:31 +0200

    🔨 Prefix removing function added.

--- examples/arrays.v
+++ examples/arrays.v
@@ -5,7 +5,7 @@
  */
 
 fn split_words (text string) {
-	for item in text.split(' ') {
+	for item in text.split(" ") {
 		println(item)
 	}
 }
@@ -15,11 +15,19 @@ fn welcome (user []string) {
 	println("Hello ${user[0]} ${user[1]}, you're ${user[2]}.")
 }
 
+fn remove_prefix (message string, prefix string) {
+
+	println(message[prefix.len..])
+
+}
+
 fn main () {
 
 	text := "Hello i'm Ness"
 	split_words(text)
 
-	welcome(['Ness', 'MC', '15'])
+	welcome(["Ness", "MC", "15"])
+
+	remove_prefix("t!help", "t!")
 	
 }
